c430de2f7a5d2e7184e3f563158f5470758e76f2
Karem Darwesh <karemdarwesh1035@gmail.com>
2024-08-11 18:10:44 +0300

    task 1 updated

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ FIFO caching """
-
+from collections import deque
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -10,4 +10,31 @@ class FIFOCache(BaseCaching):
     def __init__(self):
         """init method"""
         super().__init__()
-        self.cache_data = {}
+        self.order = deque()
+
+    def put(self, key, item):
+        """ puts new key and value on the cache system """
+        # If key or item is None, this method should not do anything.
+        if key is None or item is None:
+            return
+
+        # Update the item value if key already exists
+        if self.cache_data.get(key):
+            self.cache_data[key] = item
+            return
+
+        # if the cache is full, replace the first inserted key
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            del_item = self.order.popleft()
+            del self.cache_data[del_item]
+            print(f"DISCARD: {del_item}")
+
+        # Adding the key and item to the cache,and the key to the order
+        self.cache_data[key] = item
+        self.order.append(key)
+
+    def get(self, key):
+        """ retrive data cached """
+        if key:
+            return self.cache_data.get(key)
+        return None
